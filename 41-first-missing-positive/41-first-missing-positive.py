d757daf35985b58354e773b7bccaeb68fd849004
Shrutika252 <71875791+Shrutika252@users.noreply.github.com>
2022-05-23 00:54:58 +0530

    Time: 723 ms (92.30%), Space: 79.5 MB (12.81%) - LeetHub

---
+++ 41-first-missing-positive/41-first-missing-positive.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        x=set(nums)
+        temp=1
+        while temp in x:
+            temp+=1
+            
+            
+        return temp
+        
+        
+        
+        
+        
+            
+        
