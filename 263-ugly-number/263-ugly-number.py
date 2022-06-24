24f2443d42f30a246676eff92889b8ca5223acc0
Shrutika252 <71875791+Shrutika252@users.noreply.github.com>
2022-06-24 20:10:44 +0530

    Time: 18 ms (90.53%), Space: 13.5 MB (14.08%) - LeetHub

---
+++ 263-ugly-number/263-ugly-number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isUgly(self, n):
+        if n<=0:
+            return False
+        
+        while n!=1:
+            if n%2==0:
+                n=n//2
+            elif n%3==0:
+                 n=n//3
+            elif n%5==0:
+                 n=n//5
+            else:
+                return False
+        return True
+                
+        
