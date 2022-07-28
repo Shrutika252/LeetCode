55a82e39066c2bade6cfebd34ada0d49a81f8d7a
Shrutika Ambre <71875791+Shrutika252@users.noreply.github.com>
2022-07-28 22:32:41 +0530

    Time: 61 ms (72.38%), Space: 14.4 MB (85.64%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -4,10 +4,8 @@ class Solution:
         for ch in s:
             if ch.isalnum():
                 final=final+ch
-        print(final)
+
         res=final.lower()
-        print(res)
-        print(res[::-1])
         if res==res[::-1]:
             return True
         else:
