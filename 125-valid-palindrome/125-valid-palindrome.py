7882aefff82bab6d627a0637d30f589283602373
Shrutika Ambre <71875791+Shrutika252@users.noreply.github.com>
2022-07-28 22:32:18 +0530

    Time: 65 ms (66.13%), Space: 14.6 MB (45.71%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,8 +1,15 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        y=s.lower()
-        y=''.join (ch for ch in y if ch.isalnum())
-        if y == y[::-1]:
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        final=''
+        for ch in s:
+            if ch.isalnum():
+                final=final+ch
+        print(final)
+        res=final.lower()
+        print(res)
+        print(res[::-1])
+        if res==res[::-1]:
             return True
         else:
             return False
+        
