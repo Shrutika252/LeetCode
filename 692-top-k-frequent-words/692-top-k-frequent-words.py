659a7164fabfab5cacc82a7e7a5f16e0f15779c4
Shrutika Ambre <71875791+Shrutika252@users.noreply.github.com>
2022-07-31 23:55:03 +0530

    Time: 83 ms (61.04%), Space: 14.1 MB (27.03%) - LeetHub

---
+++ 692-top-k-frequent-words/692-top-k-frequent-words.py
@@ -0,0 +1,18 @@
+from collections import Counter
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        dict1=Counter(words)
+        sort_dict=dict(sorted(dict1.items()))
+        print(sort_dict)
+        
+        res=[]
+     
+        while k!=0:
+            new_value=max(sort_dict,key=sort_dict.get)
+            res.append(new_value)
+            del sort_dict[new_value]
+            k=k-1
+            
+            
+            
+        return res
