57e983a898742f0ae84f4c96a48ac5afabc301d3
Shrutika Ambre <71875791+Shrutika252@users.noreply.github.com>
2022-07-31 16:43:43 +0530

    Time: 804 ms (29.06%), Space: 0B (100.00%) - LeetHub

--- 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
@@ -1,1 +1,1 @@
-select customer_number from Orders group by customer_number order by count(*) desc limit 1
+select customer_number from Orders Group by customer_number order by count(*) desc limit 1
