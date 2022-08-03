de32af64112b304d7c666839c9b505c873cdab2e
Shrutika Ambre <71875791+Shrutika252@users.noreply.github.com>
2022-08-03 23:49:21 +0530

    Time: 58 ms (68.55%), Space: 13.9 MB (31.13%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.py
+++ 43-multiply-strings/43-multiply-strings.py
@@ -1,5 +1,22 @@
-class Solution(object):
-    def multiply(self, num1, num2):
-        c=int(num1)*int(num2)
-        return str(c)
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        list1=[num1,num2]
+        res=[]
+        num=0
+        
+        for i in range(len(list1)):
+            s=list1[i]
+            
+            for j in s:
+                num = num * 10 + (ord(j) - 48) 
+                
+            res.append(num)
+            num=0
+            
+        mul=1
+        for i in res:
+            mul=mul*i
+
+        return str(mul)
+        
         
