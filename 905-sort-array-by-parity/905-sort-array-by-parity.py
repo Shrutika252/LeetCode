89f21253e9373886dfbd395d99a6181238bf0608
Shrutika Ambre <71875791+Shrutika252@users.noreply.github.com>
2022-09-20 15:23:59 +0530

    Time: 201 ms (6.84%), Space: 14.7 MB (16.07%) - LeetHub

---
+++ 905-sort-array-by-parity/905-sort-array-by-parity.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        even=[]
+        odd=[]
+        for i in range(len(nums)):
+            if nums[i]%2==0:
+                even.append(nums[i])
+            else:
+                odd.append(nums[i])
+                
+        nums=even+odd
+        return nums
